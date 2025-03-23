7c0ccea987a06c156899f6e6e991d5e04513ca9a
thodorostsao <60474569+thodorostsao@users.noreply.github.com>
2025-03-23 20:19:45 +0200

    Update db.js

--- qorder/lib/db.js
+++ qorder/lib/db.js
@@ -2,9 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'doadmin',        // Προσαρμόστε τα στοιχεία σύνδεσης
-  host: 'qorderdb-do-user-20284434-0.k.db.ondigitalocean.com',       // Ή τη διεύθυνση του remote database
+  host: 'orders-do-user-20284434-0.k.db.ondigitalocean.com',       // Ή τη διεύθυνση του remote database
   database: 'defaultdb',
-  password: 'AVNS_aHPU2LmbCqh7Y-8awnP',
+  password: 'AVNS_Mza68pHYplkmahOsK7T',
   port: 25060,
 });
 
